761275d95db2b158fed182877958eb2f25787df2
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-07-24 16:02:39 +0300

    add evaluator.

--- prompt.c
+++ prompt.c
@@ -22,6 +22,33 @@ void add_history(char* unused) {}
 
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char *op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t *t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    char *op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char **argv) {
     mpc_parser_t *Number = mpc_new("number");
     mpc_parser_t *Operator = mpc_new("operator");
@@ -36,7 +63,7 @@ int main(int argc, char **argv) {
       lispy    : /^/ <operator> <expr>+ /$/ ;             \
     ", Number, Operator, Expr, Lispy);
 
-    puts("Lispy Version 0.0.0.0.2");
+    puts("Lispy Version 0.0.0.0.3");
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
@@ -45,7 +72,8 @@ int main(int argc, char **argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
